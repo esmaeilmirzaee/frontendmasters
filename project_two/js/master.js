7ed7bc7ccfbbbb88baa13c100b94ffa1c309fc98
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-05-19 06:20:34 -0400

    Finish project 2

--- project_two/js/master.js
+++ project_two/js/master.js
@@ -21,21 +21,70 @@ function handleNumber(value) {
   } else {
     buffer += value;
   }
-  reRender()
+  reRender();
 }
 
 function handleSymbol(value) {
   switch (value) {
     case "C":
-      console.log("Clear");
       buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
       break;
-  
+      case "=":
+        if (previousOperator === null) {
+          return;
+        }
+        flushOperation(parseInt(buffer));
+        previousOperator = null;
+        buffer = "" + runningTotal;
+        runningTotal = 0;
+        break;
+      case '←':
+        if (buffer.length === 1) {
+          buffer = "0";
+        } else {
+          buffer = buffer.substring(0, buffer.length - 1)
+        }
+        break;
     default:
+      handleMath(value);
       break;
   }
+  reRender();
 }
 
 function reRender() {
   screen.innerText = buffer;
+}
+
+function handleMath(value) {
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = value;
+  console.log(previousOperator);
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  switch (previousOperator) {
+    case "-":
+      runningTotal -= intBuffer;
+      break;
+    case "+":
+      runningTotal += intBuffer;
+      break;
+    case "×":
+      runningTotal *= intBuffer;
+      break;
+    case "÷":
+      runningTotal /= intBuffer;
+      break;
+  }
+  console.log(runningTotal);
 }
